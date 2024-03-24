bae3d4330fc098e13630c4746606fe6bc998dad1
LiciuStefan <licius9090@gmail.com>
2024-03-24 22:39:14 +0200

    Adding user right checking logic

--- src/main/java/ro/unibuc/hello/entities/Policy.java
+++ src/main/java/ro/unibuc/hello/entities/Policy.java
@@ -1,8 +1,8 @@
 package ro.unibuc.hello.entities;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import ro.unibuc.hello.common.EffectTypes;
 import ro.unibuc.hello.dtos.PolicyDTO;
-
 import java.util.List;
 
 @Getter
@@ -28,5 +28,18 @@ public class Policy {
 
     public PolicyDTO toDTO() {
         return new PolicyDTO(id, name, statements);
+    }
+
+    public int hasRightsToAction(String actionCode){
+        boolean hasRights;
+        for(var statement : statements) {
+            for(var statementAction : statement.getActions()) {
+                if(statementAction.equals(actionCode)) {
+                    hasRights = statement.getEffect().equals(EffectTypes.ALLOW);
+                    return hasRights ? 1 : -1;
+                }
+            }
+        }
+        return 0;
     }
 }
---
+++ src/main/java/ro/unibuc/hello/services/CheckUserRightsService.java
@@ -0,0 +1,46 @@
+package ro.unibuc.hello.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ro.unibuc.hello.exceptions.UserNotAuthorizedException;
+
+@Component
+public class CheckUserRightsService {
+
+    @Autowired
+    private UserService userService;
+
+    public Boolean checkUserRights(String id, String actionCode) {
+        boolean hasRights = false;
+        var user = userService.getUserById(id);
+        var policies = user.getPolicies();
+        for(var policy : policies) {
+            if(policy.toPolicy().hasRightsToAction(actionCode) == 1) {
+                hasRights = true;
+            }
+            if(policy.toPolicy().hasRightsToAction(actionCode) == -1) {
+                throw new UserNotAuthorizedException(user.getId());
+            }
+        }
+
+        if(hasRights) {
+            return true;
+        }
+
+        var roles = user.getRoles();
+        for(var role : roles) {
+            for(var policy : role.getPolicies()) {
+                if(policy.toPolicy().hasRightsToAction(actionCode) == 1) {
+                    hasRights = true;
+                }
+                if(policy.toPolicy().hasRightsToAction(actionCode) == -1) {
+                    throw new UserNotAuthorizedException(user.getId());
+                }
+            }
+        }
+
+        return hasRights;
+    }
+
+}
