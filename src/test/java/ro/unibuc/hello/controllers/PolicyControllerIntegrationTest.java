fd591d2bb127fb0416745ecc223dc92d10415c99
LiciuStefan <licius9090@gmail.com>
2024-04-04 10:28:10 +0300

    Adding integration test schema for the policy controller

---
+++ src/test/java/ro/unibuc/hello/controllers/PolicyControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package ro.unibuc.hello.controllers;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class PolicyControllerIntegrationTest {
+}
