1bc086188ba4db348cb2b7e0af813cf318359db9
Victor Mura <victormura.s@gmail.com>
2024-04-18 10:38:31 +0300

    Fix localhost

--- src/test/java/ro/unibuc/hello/e2e/steps/HelloWorldSteps.java
+++ src/test/java/ro/unibuc/hello/e2e/steps/HelloWorldSteps.java
@@ -34,7 +34,7 @@ public class HelloWorldSteps {
 
     @Given("^the client calls /hello-world")
     public void the_client_issues_GET_hello() {
-        executeGet("http://localhost:8080/hello-world");
+        executeGet("http://host.docker.internal:8080/hello-world");
     }
 
     @Then("^the client receives status code of (\\d+)$")
